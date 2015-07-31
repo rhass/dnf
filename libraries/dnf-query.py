b01e846c7061dfc7d8cb078adc72ac4d7ee339e4
joe miller <joeym@joeym.net>
2015-07-31 14:41:53 +0000

    only return pkgs matching one arch, either the detected arch or the explicitly requested arch

--- libraries/dnf-query.py
+++ libraries/dnf-query.py
@@ -9,6 +9,9 @@
 # default behavior # is to show only the latest version. This can
 # be changed with `--latest-limit=N`.
 #
+# Package names should match <package-spec> format here:
+# http://dnf.readthedocs.org/en/latest/command_ref.html#specifying-packages-label
+#
 # Examples:
 #
 #   $ dnf-query.py tomcat
@@ -31,8 +34,6 @@
 #   3.8.10.2-1.fc22.i686
 #
 #   $ dnf-query.py sqlite '>= 3.7' --latest-limit=3
-#   3.8.10.2-1.fc22.i686
-#   3.8.9-1.fc22.i686
 #   3.8.10.2-1.fc22.x86_64
 #   3.8.9-1.fc22.x86_64
 #
@@ -41,11 +42,11 @@
 #   3.8.9-1.fc22.i686
 #
 #   $ dnf-query.py sqlite '>= 3.7' --latest-limit=1
-#   3.8.10.2-1.fc22.i686
 #   3.8.10.2-1.fc22.x86_64
 #
 
 import dnf
+import hawkey
 
 # map human readable comparison operators to dnf's query language
 # http://dnf.readthedocs.org/en/latest/api_queries.html#dnf.query.Query.filter
@@ -67,7 +68,6 @@ def get_sack():
 
 
 def run(args):
-    name = args.name
     comparator = args.comparator
     version = None
     release = None
@@ -81,27 +81,43 @@ def run(args):
 
     sack = get_sack()
 
-    q = dnf.subject.Subject(name).get_best_query(sack)
+    subj = dnf.subject.Subject(args.pkg_spec)
+
+    q = subj.get_best_query(sack)
+    q_kwargs = {}
+
+    # We only show versions of a package matching a specific architecture, but provide support for matching
+    # special 'noarch' packages as well.
+    #
+    # For example:
+    # - if pkg_spec is 'systemd' and detected arch is x86_64, return 'systemd.x86_64' packages.
+    # - if pkg_spec is 'systemd.i686', return 'systemd.i686' packages regardless of detected arch.
+    # - if pkg_spec is 'tomcat', return 'tomcat.noarch' packages since there is no x86_64 version.
+    #
+    requested_arch = dnf.util.first(subj.subj.nevra_possibilities_real(sack, allow_globs=True)).arch
+    if requested_arch:
+        q_kwargs['arch'] = ['noarch', requested_arch]
+    else:
+        q_kwargs['arch'] = ['noarch', hawkey.detect_arch()]
 
     if args.installed:
         q = q.installed()
     else:
         q = q.available()
 
-    kwargs = {}
     if version:
-        kwargs['version__{}'.format(COMPARATOR_MAP[comparator])] = version
+        q_kwargs['version__{}'.format(COMPARATOR_MAP[args.comparator])] = version
     if release:
-        kwargs['release__{}'.format(COMPARATOR_MAP[comparator])] = release
-    q = q.filter(**kwargs)
+        q_kwargs['release__{}'.format(COMPARATOR_MAP[args.comparator])] = release
+
+    q = q.filter(**q_kwargs)
 
     pkgs = dnf.query.latest_limit_pkgs(q, args.latest_limit)
-
     for pkg in pkgs:
         if args.print_name:
-            print '{}-{}:{}-{}'.format(pkg.name, pkg.epoch, pkg.version, pkg.release)
+            print '{}-{}:{}-{}.{}'.format(pkg.name, pkg.epoch, pkg.version, pkg.release, pkg.arch)
         else:
-            print '{}:{}-{}'.format(pkg.epoch, pkg.version, pkg.release)
+            print '{}:{}-{}.{}'.format(pkg.epoch, pkg.version, pkg.release, pkg.arch)
 
 
 if __name__ == '__main__':
@@ -111,7 +127,7 @@ if __name__ == '__main__':
     #             since the dnf provider (inherited from yum) has some support
     #             for passing repo opts to dnf?
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('name', nargs='?',
+    parser.add_argument('pkg_spec', nargs='?',
                         help='The package name to query.')
     parser.add_argument('comparator', nargs='?',
                         help='comparsion function: =, >, >=, <, <=')
@@ -121,7 +137,7 @@ if __name__ == '__main__':
     parser.add_argument('--installed', action='store_true',
                        help='List installed packages instead of available packages.')
     parser.add_argument('--latest-limit', dest='latest_limit', type=int, default=1,
-                       help='Show latest N matching patckages.')
+                       help='Show latest N matching packages.')
     parser.add_argument('--print-name', dest='print_name', action='store_true',
                        help='Print package name(s).')
     args = parser.parse_args()
